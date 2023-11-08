927273c2ab7b32dcbdb4d4df8461035f139c97b3
GkhKaya <gkhkaya00@gmail.com>
2023-11-08 15:08:16 +0300

    First Commit

---
+++ SwiftUiBarCharts/SwiftUiBarChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiBarChartsApp.swift
+//  SwiftUiBarCharts
+//
+//  Created by Gokhan Kaya on 8.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiBarChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
